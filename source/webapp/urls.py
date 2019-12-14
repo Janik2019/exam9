c2e800e108f3838ee5279741b55fd09c482d8c9b
janik2019 <lucky-lucky-7777@mail.ru>
2019-12-14 17:16:45 +0600

    добавил вход и выход на базе loginview logout view

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import IndexView, PhotoView, PhotoCreateView, PhotoUpdateView, PhotoDeleteView
+from django.contrib.auth.views import LoginView, LogoutView
 
 
 app_name = 'webapp'
@@ -10,4 +11,6 @@ urlpatterns = [
     path('photo/create/', PhotoCreateView.as_view(), name='photo_create'),
     path('photo/<int:pk>/update/', PhotoUpdateView.as_view(), name='photo_update'),
     path('photo/<int:pk>/delete/', PhotoDeleteView.as_view(), name='photo_delete'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout')
 ]
